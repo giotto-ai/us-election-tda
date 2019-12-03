78948f517f26e2ebd8aeb84773efcd40d8fef820
Philipp Weiler <p.weiler@l2f.ch>
2019-12-03 08:40:00 +0100

    add function to plot the mapper result with plotly

--- plotting.py
+++ plotting.py
@@ -2,6 +2,10 @@ import networkx as nx
 import numpy as np
 import plotly.figure_factory as ff
 
+import plotly.graph_objects as go
+from functools import reduce
+import operator
+
 
 def nx_plot_mapper(mapper, node_color=None, cmap='autumn',
                    node_size=None, pos=None, with_labels=False,
@@ -24,4 +28,64 @@ def county_plot(fips, values, colorscale=["#0000ff", "#ff0000"],
                                asp=2.9, title='Election Outcome',
                                legend_title=legend_title)
     fig.layout.template = None
+    fig.show()
+
+
+def plot_mapper(mapper, df, pos, size, node_color, node_text, col='winner',
+                colorscale='RdBu', title='Percentage voted for republican'):
+    edge_x = list(reduce(operator.iconcat,
+                  map(lambda x: [pos[x[0]][0],
+                                 pos[x[1]][0], None],
+                      mapper.complex._graph.edges()), []))
+    edge_y = list(reduce(operator.iconcat,
+                  map(lambda x: [pos[x[0]][1],
+                                 pos[x[1]][1], None],
+                      mapper.complex._graph.edges()), []))
+
+    edge_trace = go.Scatter(
+            x=edge_x, y=edge_y,
+            line=dict(width=0.5, color='#888'),
+            hoverinfo='none',
+            mode='lines')
+
+    node_x = list(map(lambda x: pos[x][0], range(len(pos))))
+    node_y = list(map(lambda x: pos[x][1], range(len(pos))))
+
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers',
+        hoverinfo='text',
+        marker=dict(
+            showscale=True,
+            colorscale=colorscale,
+            reversescale=True,
+            color=[],
+            size=size,
+            cmax=1,
+            cmin=0,
+            colorbar=dict(
+                thickness=15,
+                title=title,
+                xanchor='left',
+                titleside='right'
+            ),
+            line_width=2))
+
+    node_trace.marker.color = node_color
+
+    node_trace.text = node_text
+
+    fig = go.Figure(data=[edge_trace, node_trace],
+                    layout=go.Layout(showlegend=False,
+                                     hovermode='closest',
+                                     margin=dict(b=20, l=5, r=5, t=40),
+                                     xaxis=dict(showgrid=False, zeroline=False,
+                                                showticklabels=False, ticks="",
+                                                showline=False),
+                                     yaxis=dict(showgrid=False, zeroline=False,
+                                                showticklabels=False, ticks="",
+                                                showline=False),
+                                     xaxis_title="",
+                                     yaxis_title=""))
+    fig.update_layout(template='simple_white')
     fig.show()
