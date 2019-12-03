3ac95d0781bc612b21e5321b2ecbff5e846503a0
Philipp Weiler <p.weiler@l2f.ch>
2019-12-03 07:14:19 +0100

    add function to calculate county plot data

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.colors
+
 import sys
 sys.path.append('..')
 import lmapper as lm
@@ -25,3 +29,23 @@ def get_mapper(filtr, nintervals, overlap, method,
                        filter=filtr,
                        cover=cover, cluster=cluster)
     return mapper
+
+
+def get_county_plot_data(data, mapper, df, col, cmap):
+    map_col = (pd.DataFrame(np.zeros((data.shape[0],
+                                      2)),
+                            columns=['color_sum', 'n_counties'])
+               .astype({'n_counties': 'int'}))
+
+    for rows, val in zip(list(map(lambda x: x._labels.tolist(),
+                                  dict(mapper._nodes.items()).values())),
+                         get_mean_node(mapper, df=df,
+                                       col=col)):
+        map_col.loc[rows] = map_col.loc[rows] + [val, 1]
+
+    colors = list(map(lambda x: matplotlib.colors.rgb2hex(x),
+                      cmap((map_col['color_sum'] /
+                            map_col['n_counties']).unique().tolist())[:, :3]))
+
+    return ((map_col['color_sum'] / map_col['n_counties']).tolist(),
+            colors)
