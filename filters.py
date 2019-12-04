b5805014ab235eb13dc382956bda7eef578897f0
Philipp Weiler <p.weiler@l2f.ch>
2019-12-04 08:23:45 +0100

    add filters that return a transformation of svd or projection

--- filters.py
+++ filters.py
@@ -11,6 +11,31 @@ class SvdFilter(Filter):
         return self.pca.fit_transform(data).flatten()
 
 
+class LogSvdFilter(Filter):
+    def __init__(self, n_components):
+        self.pca = PCA(n_components=n_components)
+
+    def __call__(self, data):
+        res = self.pca.fit_transform(data).flatten()
+        return np.log(res + abs(np.min(res)) + 1)
+
+
+class TrafoSvdFilter(Filter):
+    def __init__(self, n_components):
+        self.pca = PCA(n_components=n_components)
+
+    def __call__(self, data):
+        return np.log(np.abs(self.pca.fit_transform(data).flatten()))
+
+
+class LogProjectionFilter(Filter):
+    def __init__(self, ax):
+        self.ax = ax
+
+    def __call__(self, data):
+        return np.log(data[:, self.ax] + abs(np.min(data[:, self.ax])) + 1)
+
+
 class LpFilter(Filter):
     def __init__(self, p, k):
         self.p = p
