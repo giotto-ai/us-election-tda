b4274b9f07086bedf5a9053f1bad74718a7462a6
Philipp Weiler <p.weiler@l2f.ch>
2019-12-04 11:53:29 +0100

    add data creation and transformation functions; add an size_offset keyword argument to the get_weighted_electors_plot function

--- utils.py
+++ utils.py
@@ -8,6 +8,8 @@ import networkx as nx
 
 from plotting import mapper_plotly_plot
 
+from sklearn.preprocessing import StandardScaler
+
 import sys
 sys.path.append('..')
 import lmapper as lm
@@ -74,10 +76,10 @@ def get_unique_cluster_vals(mapper, cluster):
                                             ), []))))
 
 
-def get_weighted_electors_plot(mapper, df, seed=0):
+def get_weighted_electors_plot(mapper, df, seed=0, size_offset=12):
     col = 'winner'
     pos = nx.spring_layout(mapper.complex._graph, seed=seed)
-    size = list(map(lambda x: 12 + x / min(get_node_size(mapper)),
+    size = list(map(lambda x: size_offset + x / min(get_node_size(mapper)),
                     get_node_size(mapper)))
 
     node_color = list(map(lambda x, y: x / y,
@@ -105,4 +107,93 @@ def get_weighted_electors_plot(mapper, df, seed=0):
     cmax = np.max(node_color)
     mapper_plotly_plot(mapper, df, pos, size, node_color, node_text, cmin=cmin,
                        cmax=cmax, colorscale='RdBu',
-                       title='Percentage voted for republican')
+                       title='Percentage Voted for Republican')
+
+
+def get_cols_by_type():
+    num_cols = ['Personal income (thousands of dollars)',
+                'Net earnings by place of residence',
+                'Personal current transfer receipts',
+                'Income maintenance benefits 1/',
+                'Unemployment insurance compensation',
+                'Retirement and other',
+                'Dividends, interest, and rent 2/',
+                'Population (persons) 3/',
+                'Per capita personal income 4/',
+                'Per capita net earnings 4/',
+                'Per capita personal current transfer receipts 4/',
+                'Per capita income maintenance benefits 4/',
+                'Per capita unemployment insurance compensation 4/',
+                'Per capita retirement and other 4/',
+                'Per capita dividends, interest, and rent 4/',
+                'Earnings by place of work',
+                'Wages and salaries',
+                'Supplements to wages and salaries',
+                'Employer contributions for employee pension and ' +
+                'insurance funds 5/',
+                'Employer contributions for government social insurance',
+                "Proprietors' income",
+                "Farm proprietors' income",
+                "Nonfarm proprietors' income",
+                'Total employment (number of jobs)',
+                'Wage and salary employment',
+                'Proprietors employment',
+                'Farm proprietors employment 6/',
+                'Nonfarm proprietors employment',
+                'Average earnings per job (dollars)',
+                'Average wages and salaries',
+                "Average nonfarm proprietors' income"]
+
+    info_cols = ['year', 'state', 'county', 'fips', 'pres']
+
+    elec_cols = ['republican', 'democrat', 'total_votes', 'n_electors',
+                 'winner']
+
+    return num_cols, info_cols, elec_cols
+
+
+def get_data(df):
+    num_cols, _, _ = get_cols_by_type()
+
+    # perform a log transformation on the data
+    df[num_cols] = (df[num_cols] +
+                    abs(df[num_cols].min().min()) + 1).apply(np.log)
+
+    # columns to use for the mapper were found by comparing the distribution
+    # of each column between the different election years
+    # The ones differing throughout the years are selected (selection was made
+    # by eye)
+    cols2use = ['Personal income (thousands of dollars)',
+                'Net earnings by place of residence',
+                'Income maintenance benefits 1/',
+                'Unemployment insurance compensation',
+                'Per capita personal income 4/',
+                'Per capita net earnings 4/',
+                'Per capita personal current transfer receipts 4/',
+                'Per capita income maintenance benefits 4/',
+                'Per capita unemployment insurance compensation 4/',
+                'Per capita retirement and other 4/',
+                'Per capita dividends, interest, and rent 4/',
+                'Earnings by place of work',
+                "Proprietors' income",
+                "Nonfarm proprietors' income",
+                'Total employment (number of jobs)',
+                'Proprietors employment',
+                'Farm proprietors employment 6/',
+                'Nonfarm proprietors employment',
+                'Average earnings per job (dollars)',
+                'Average wages and salaries',
+                "Average nonfarm proprietors' income"]
+
+    # scale data to have zero mean and a standard deviation of one
+    scaler = StandardScaler()
+    df[num_cols] = scaler.fit_transform(df[num_cols])
+
+    return df[cols2use].values
+
+
+def split_data_by_year(data, df):
+    return dict(zip(['2000', '2004', '2008', '2012', '2016'],
+                map(lambda x: data[x, :],
+                    map(lambda x: df[df['year'] == x].index,
+                        df['year'].unique()))))
