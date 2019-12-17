081add55bc92143cb98a2d133f539b80b1518d4d
Philipp Weiler <p.weiler@l2f.ch>
2019-12-17 14:08:02 +0100

    add label argument to node text

--- utils.py
+++ utils.py
@@ -108,10 +108,10 @@ def get_n_electors(node_elements, n_electors):
             for x in node_elements]
 
 
-def get_node_text(node_elements, n_electors, node_color):
+def get_node_text(node_elements, n_electors, node_color, label):
     return [f'Node Id: {x[0]}<br>Node size: {len(x[1])}<br>' +
             f'Number of Electors: {y}<br>'
-            f'Mean Value: {z}'
+            f'Mean {label}: {z}'
             for x, y, z in zip(node_elements.items(), n_electors, node_color)]
 
 
