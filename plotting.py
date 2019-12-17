fe21149dbb9c453a9b3564a8bb70367939606a2a
Philipp Weiler <p.weiler@l2f.ch>
2019-12-17 14:09:02 +0100

    remove function

--- plotting.py
+++ plotting.py
@@ -1,21 +1,6 @@
-import plotly.figure_factory as ff
 import utils
 import numpy as np
 from giotto.mapper import visualize
-
-
-def county_plot(fips, values, colorscale=["#0000ff", "#ff0000"],
-                legend_title='Republican/Democrat'):
-    # https://plot.ly/python/county-choropleth/
-
-    fig = ff.create_choropleth(fips=fips, values=values,
-                               colorscale=colorscale,
-                               show_state_data=False,
-                               show_hover=True, centroid_marker={'opacity': 0},
-                               asp=2.9, title='Election Outcome',
-                               legend_title=legend_title)
-    fig.layout.template = None
-    fig.show()
 
 
 def get_graph_plot_colored_by_winner(graph, year, df, pos=None,
