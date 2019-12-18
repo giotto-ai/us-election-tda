6c0cfac742a067235b73ed886982a845e8113db1
Philipp Weiler <p.weiler@l2f.ch>
2019-12-18 09:12:02 +0100

    add county plot function

--- plotting.py
+++ plotting.py
@@ -1,5 +1,20 @@
+import plotly.figure_factory as ff
 import utils
 from giotto.mapper import visualize
+
+
+def county_plot(fips, values, colorscale=["#0000ff", "#ff0000"],
+                legend_title='Republican/Democrat'):
+    # https://plot.ly/python/county-choropleth/		
+
+    fig = ff.create_choropleth(fips=fips, values=values,
+                               colorscale=colorscale,
+                               show_state_data=False,
+                               show_hover=True, centroid_marker={'opacity': 0},
+                               asp=2.9, title='Election Outcome',
+                               legend_title=legend_title)
+    fig.layout.template = None
+    fig.show()
 
 
 def get_graph_plot_colored_by_election_results(graph, year, df, data,
