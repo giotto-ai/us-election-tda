d01fbc020e5e5a35c76667b360f5464a09e26d41
Philipp Weiler <p.weiler@l2f.ch>
2020-01-07 12:13:45 +0100

    add explanatory comments

--- plotting.py
+++ plotting.py
@@ -10,6 +10,12 @@ def get_region_plot(graph, data, layout, columns_to_color, node_elements,
                     colorscale):
     regions = utils.get_regions()
 
+    # set node color:
+    # 1. assign to each node of a region its color (zip())
+    # 2. convert zip elements to list (map())
+    # 3. flatten list (itertools.chain())
+    # 4. sort values by keys
+    # 5. convert to ordered dictionary
     node_color = list(
         collections.OrderedDict(
             sorted(itertools.chain(
@@ -18,6 +24,9 @@ def get_region_plot(graph, data, layout, columns_to_color, node_elements,
                           itertools.repeat(colorscale[region]))
                       for region in range(len(regions))])))).values())
 
+    # set plotly arguments:
+    # 1. set uniform node size
+    # 2. hide scale of marker color
     plotly_kwargs = {
         'node_trace_marker_size': [1] * len(node_elements),
         'node_trace_marker_showscale': False}
@@ -34,6 +43,7 @@ def get_graph_plot_colored_by_election_results(graph, year, df, data,
     if layout is None:
         layout = graph.layout('kk', dim=2)
 
+    # set node color to percentage of number of electors won by republicans
     node_color = [
         100 * (df[df['year'] == year]['winner'].values *
                df[df['year'] == year]['n_electors'].values)[x].sum() /
@@ -88,8 +98,10 @@ def get_county_plot(fips, values, colorscale=["#0000ff", "#ff0000"],
 
 
 def get_county_plot_by_region(data, colorscale, node_elements, fips):
+    # convert colorscale from hex format to rgb
     colorscale = dict(zip(map(str, range(len(colorscale))),
                           utils.hex2rgb(colorscale.values())))
+    # define color of counties belonging to two regions as their mean rgb value
     colorscale['1-3'] = utils.mean_rgb([colorscale['1'],
                                         colorscale['3']])
     colorscale['2-3'] = utils.mean_rgb([colorscale['2'],
@@ -102,28 +114,40 @@ def get_county_plot_by_region(data, colorscale, node_elements, fips):
     elements_per_region = utils.get_data_per_region(utils.get_regions(),
                                                     node_elements)
 
+    # assign each county its color
     county_color = np.zeros(data.shape[0], dtype='int')
+    # region 0
     county_color[list(elements_per_region[0])] = 0
+    # region 1 and not 3
     county_color[list(elements_per_region[1]
                       .difference(elements_per_region[3]))] = 1
+    # region 2 and not 3
     county_color[list(elements_per_region[2]
                       .difference(elements_per_region[3]))] = 2
+    # region 3 and neither 1 nor 2
     county_color[list(elements_per_region[3]
                       .difference(elements_per_region[1])
                       .difference(elements_per_region[2]))] = 3
+    # region 4 but not 3
     county_color[list(elements_per_region[4]
                       .difference(elements_per_region[3]))] = 4
+    # region 5 but not 4
     county_color[list(elements_per_region[5]
                       .difference(elements_per_region[4]))] = 5
 
+    # region 1 and 3
     county_color[list(elements_per_region[1]
                       .intersection(elements_per_region[3]))] = 6
+    # region 2 and 3
     county_color[list(elements_per_region[2]
                       .intersection(elements_per_region[3]))] = 7
+    # region 3 and 4
     county_color[list(elements_per_region[3]
                       .intersection(elements_per_region[4]))] = 8
+    # region 4 and 5
     county_color[list(elements_per_region[4]
                       .intersection(elements_per_region[5]))] = 9
+
     county_color = county_color.tolist()
 
     return get_county_plot(
