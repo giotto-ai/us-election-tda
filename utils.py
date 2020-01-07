09ddc326a06b960d8c439ce103c9007e9ca71f64
Philipp Weiler <p.weiler@l2f.ch>
2020-01-07 13:52:41 +0100

    add explanatory comments

--- utils.py
+++ utils.py
@@ -7,6 +7,15 @@ import pandas as pd
 
 
 def get_cols_by_type():
+    '''
+        Function to get different columns
+        Input:
+        Output:
+            Tuple of lists of
+            1. columns with numerical values,
+            2. columns with general information,
+            3. columns related to presidential election
+    '''
     num_cols = ['Personal income (thousands of dollars)',
                 'Net earnings by place of residence',
                 'Personal current transfer receipts',
@@ -49,10 +58,15 @@ def get_cols_by_type():
 
 
 def get_cols_for_mapper():
-    # columns to use for the mapper were found by comparing the distribution
-    # of each column between the different election years
-    # The ones differing throughout the years are selected (selection was made
-    # by eye)
+    '''
+        Function to return columns used for Mapper Algorithm. Columns to use
+        for the mapper were found by comparing the distribution of each column
+        between the different election years. The ones differing throughout the
+        years are selected (selection was made by eye)
+        Input:
+        Output:
+            List of columns used for Mapper algorithm
+    '''
     return ['Personal income (thousands of dollars)',
             'Net earnings by place of residence',
             'Income maintenance benefits',
@@ -77,6 +91,13 @@ def get_cols_for_mapper():
 
 
 def get_data(df):
+    '''
+        Function to extract data for Mapper from data frame.
+        Input:
+            df: Pandas data frame containing
+        Output:
+            Scaled relevant data values (ndarray)
+    '''
     data_cols = get_cols_for_mapper()
 
     # perform a log transformation on the data
@@ -91,6 +112,15 @@ def get_data(df):
 
 
 def split_data_by_year(data, df):
+    '''
+        Function to split entire data into different election years.
+        Input:
+            data: Mapper input (ndarray)
+            df: Pandas data frame of entire data
+        Output:
+            dictionary with election year as key and corresponding economic
+            data as values
+    '''
     return dict(zip(['2000', '2004', '2008', '2012', '2016'],
                 map(lambda x: data[x, :],
                     map(lambda x: df[df['year'] == x].index,
@@ -98,6 +128,13 @@ def split_data_by_year(data, df):
 
 
 def filter_2d_trafo(x):
+    '''
+        Transformation of PCA values to obtain final filter.
+        Input:
+            x: Filter values (ndarray)
+        Output:
+            Transformed PCA (ndarray)
+    '''
     x[:, 0] = np.log(x[:, 0] - min(x[:, 0]) + 1)
     x[:, 1] = np.log(np.abs(x[:, 1]) + 1)
     return x
@@ -105,20 +142,61 @@ def filter_2d_trafo(x):
 
 # for plotting
 def get_node_size(node_elements):
+    '''
+        Function to get node size
+        Input:
+            node_elements: List/tuple containing, for each node, a list like
+                information on all data points belonging to node
+        Output:
+            List of node sizes
+    '''
     return list(map(len, node_elements))
 
 
 def get_node_summary(node_elements, data, summary_stat=np.mean):
+    '''
+        Function to calculate a summary statistic per node
+        Input:
+            node_elements: List/tuple containing, for each node, a list like
+                information on all data points belonging to node
+            data: Data to be used (ndarray)
+            summary_stat: Summary statistic (default: np.mean)
+        Output:
+            List of summary statistics
+    '''
     return list(map(lambda x: summary_stat(data[x]),
                     node_elements))
 
 
 def get_n_electors(node_elements, n_electors):
+    '''
+        Function to calculate percentage of electors belonging to each node
+        Input:
+            node_elements: List/tuple containing, for each node, a list like
+                information on all data points belonging to node
+            n_electors: Pandas series of number of weighted electors per
+                county
+        Output:
+            List of percentage of electors within a node(w.r.t to total number
+            of electors)
+    '''
     return [100 * n_electors.iloc[x].sum() / n_electors.sum()
             for x in node_elements]
 
 
 def get_node_text(node_elements, n_electors, node_color, label):
+    '''
+        Function to create text of node label
+        Input:
+            node_elements: List/tuple containing, for each node, a list like
+                information on all data points belonging to node
+            n_electors: Pandas series of number of weighted electors per
+                county
+            node_color: List of node colors
+            label: Name of label (e.g. 'income') (str)
+        Output:
+            List of text for node labels
+    '''
     return [f'Node Id: {x[0]}<br>' +
             f'Node size: {len(x[1])}<br>' +
             f'Percentage of Weighted Electors: {round(y, 2)}<br>' +
@@ -129,6 +207,16 @@ def get_node_text(node_elements, n_electors, node_color, label):
 
 
 def get_subgraph(graph, vertices_to_remove):
+    '''
+        Extract a subgraph out of a given one.
+        Input:
+            graph: igraph object
+            vertices_to_remove: List of vertices (defined by node id) to
+                remove from graph
+        Output:
+            An igraph object containing all but specified vertices (and
+            corresponding edges)
+    '''
     subgraph = graph.copy()
     subgraph.delete_vertices(vertices_to_remove)
 
@@ -136,6 +224,17 @@ def get_subgraph(graph, vertices_to_remove):
 
 
 def get_county_plot_data(graph, df, col, cmap):
+    '''
+        Function to create data for a plot of a map of the US.
+        Input:
+            graph: igraph object
+            df: Pandas dataframe
+            col: Column to base color of map on
+            cmap: Colormap
+        Output:
+            tuple of list of color of a node (numerical value) and list of
+            colors to use
+    '''
     map_col = (pd.DataFrame(np.zeros((df.shape[0],
                                       2)),
                             columns=['color_sum', 'n_counties'])
@@ -157,6 +256,13 @@ def get_county_plot_data(graph, df, col, cmap):
 
 
 def get_regions():
+    '''
+        Function to get different regions
+        Input:
+        Output:
+            Dictionary with region id as key and sets of node ids belonging to
+            them
+    '''
     return {
         0: {45, 18, 1, 7, 52, 55, 50, 49, 46, 51, 47, 30, 2, 44,
             37, 54, 53, 9, 48, 13, 24},
@@ -169,20 +275,43 @@ def get_regions():
 
 
 def get_data_per_region(regions, node_elements):
-    # return dictionary with regions as key and corresponding data points as
-    # values
+    '''
+        Function to assign to each region the data points enclosed in it.
+        Input:
+            regions: Dictionary of regions with ids as keys and set of nodes
+                as values
+            node_elements: List/tuple of data points per node
+        Output:
+            Dictionary with region ids as keys and corresponding data point ids
+            as values
+    '''
+    # create tuples of region id with corresponding data points. The latter are
+    # found by taking the union of all elements belonging to a region.
     return dict((region_id, set.union(*[set(node_elements[node])
                                         for node in regions[region_id]]))
                 for region_id in regions.keys())
 
 
 def hex2rgb(hex_colors):
-    # convert #xxyyzz hex color format to (r, g, b)
+    '''
+        Function to convert a hexa coded color into RGB format.
+        Input:
+            hex_colors: List of hexa color codes of the format '#xxyyzz'
+        Output:
+            List of tuples representing RGB codes
+    '''
     rgb_colors = [color.lstrip('#') for color in hex_colors]
     return [tuple(int(color[i:i + 2], 16) for i in (0, 2, 4))
             for color in rgb_colors]
 
 
 def mean_rgb(rgb_vals):
+    '''
+        Function to find the elementwise mean of a list of tuples.
+        Input:
+            rgb_vals: List of tuples of same length
+        Output:
+            Tuple containing the mean along of each entry
+    '''
     # calculate mean of list of rbg values
     return tuple(map(int, np.mean(rgb_vals, axis=0)))
